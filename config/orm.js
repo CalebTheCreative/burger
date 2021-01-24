0e1568ecb5b30f5694dbc45d823f0620eaec1139
Caleb Hopkins <CalebHopkins@outlook.com>
2021-01-24 14:25:16 -0600

    Added helper functions to orm.

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,43 @@
 const connection = require("../config/connection.js");
+
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+  
+    for (var key in ob) {
+      var value = ob[key];
+
+      if (Object.hasOwnProperty.call(ob, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+    return arr.toString();
+  }
+
+let orm = {
+    selectAll: function() {
+
+    },
+    insertOne: function() {
+
+    },
+    updateOne: function() {
+
+    }
+};
+
+module.exports = orm;
