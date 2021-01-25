c132a6a4c4234dfad121d527bcda5d97348a6c58
Caleb Hopkins <CalebHopkins@outlook.com>
2021-01-25 15:19:24 -0600

    Updated 'insertOne' function.

--- config/orm.js
+++ config/orm.js
@@ -2,51 +2,66 @@ const connection = require("../config/connection.js");
 
 // Helper function for SQL syntax.
 function printQuestionMarks(num) {
-    var arr = [];
-  
-    for (var i = 0; i < num; i++) {
-      arr.push("?");
-    }
-  
-    return arr.toString();
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
   }
+
+  return arr.toString();
+}
 
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
-    var arr = [];
-  
-    for (var key in ob) {
-      var value = ob[key];
+  var arr = [];
 
-      if (Object.hasOwnProperty.call(ob, key)) {
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
-        }
-        arr.push(key + "=" + value);
+  for (var key in ob) {
+    var value = ob[key];
+
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
       }
+      arr.push(key + "=" + value);
     }
-    return arr.toString();
   }
+  return arr.toString();
+}
 
 // Object for all SQL statement functions
 const orm = {
 
-    selectAll: function(tblName, cb) {
-      let queryString = "SELECT * FROM " + tblName + ";";
+  selectAll: function (tblName, cb) {
+    let queryString = "SELECT * FROM " + tblName + ";";
 
-      connection.query(queryString, function(err, result) {
-        if (err) {
-          throw err;
-        }
-        cb(result);
-      });
-    },
-    insertOne: function() {
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function (tbl, cols, vals, cb) {
+    let queryString = "INSERT INTO " + tble;
 
-    },
-    updateOne: function() {
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
 
-    }
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result)
+    })
+  },
+  updateOne: function () {
+
+  }
 };
 
 module.exports = orm;
