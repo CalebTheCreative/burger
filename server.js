4fa5b933fa1e5fcdd9a22520a087bb7a65ade986
Caleb Hopkins <CalebHopkins@outlook.com>
2021-01-26 15:59:32 -0600

    Added more dependencies.

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
+// Dependencies
+// =============================================================
 const express = require("express");
+const PORT = process.env.PORT || 9080;
+const app = express();
