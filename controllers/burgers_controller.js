4c8f089556a0dc7356144b1ac28a4b94016aab5a
Caleb Hopkins <CalebHopkins@outlook.com>
2021-01-26 15:17:05 -0600

    Added 'get route'.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,20 @@
+// Dependencies
+// =============================================================
 const express = require("express");
 const router = express.Router();
 const burger = require("../models/burger.js");
 
+// Get Routes
+// =============================================================
+router.get("/", function(req,res) {
+    burger.selectAll(function(data) {
+        let handlebars_obj = {
+            burgers: data
+        };
+
+        res.render("index", handlebars_obj);
+    });
+});
 
 
 module.exports = router;
