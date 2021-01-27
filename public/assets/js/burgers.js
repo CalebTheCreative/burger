93c857604c45fa0eb3e20d4e92c23851f2cc2273
Caleb Hopkins <CalebHopkins@outlook.com>
2021-01-26 23:54:00 -0600

    .

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,39 @@
+$(function () {
+    $(".change-eaten").on("click", function (event) {
+        let id = $(this).data("id");
+        let newEaten = $(this).data("neweaten");
+
+        let eatenStatus = {
+            devoured: newEaten
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type: PUT,
+            data: eatenStatus
+        }).then(
+            function() {
+                console.log("Set eaten to ", newEaten);
+
+                location.reload();
+            }
+        );
+    });
+
+    $(".createBurger").on("submit", function(event) {
+        event.preventDefault();
+
+        let addedBurger = {
+            burger_name: $("#createB").val().trim()
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: addedBurger
+        }).then(
+            function() {
+                console.log("Added burger to list");
+                location.reload();
+            }
+        )
+    })
+})
