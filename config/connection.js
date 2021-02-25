3114d16cce06dc60aca90e13e88680cd2bd4e2d0
Caleb Hopkins <CalebHopkins@outlook.com>
2021-02-25 12:36:32 -0600

    Updated to include JAWS_DB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySql
 const mysql = require("mysql");
 
-let connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Hookem13',
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'Hookem13',
+        database: "burgers_db"
+    });
+};
 
 // Start connection
 connection.connect(function(err) {
